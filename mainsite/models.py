05a52e74a8d8a55e85ec7abfe9de4b5923a1c5a8
Jinbangfang <hqfjb5271@126.com>
2018-03-24 15:00:22 +0800

    first commit

--- mainsite/models.py
+++ mainsite/models.py
@@ -7,10 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
     body = models.TextField()
-    pub_date = models.DateTimeField(default=timezone.now)
+    pub_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('-pub_date',)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
